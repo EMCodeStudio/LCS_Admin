53bf94ca9597b6ee283bb9fc55785663ad53e35c
EMCodeStudio <estivenmc53@gmail.com>
2024-02-10 12:35:44 -0500

    fixing total price location hook

--- lcs-admin-app/src/hooks/OrderHooks/TotalShippingPriceHook.ts
+++ lcs-admin-app/src/hooks/OrderHooks/TotalShippingPriceHook.ts
@@ -9,9 +9,9 @@ const getTotalShippingOrder: FieldHook = async ({ data, originalDoc }) => {
     try {
 
         if (data && data.TipoVentaPedido === 'product') {
-            const { CantidadProductoPedido, PrecioEnvioPedido } = data.DetallesPagoPedido
+            const { CantidadProductoPedido } = data.DetallesPagoPedido
             const stateApprovalField = data.AprobacionEstadoPedido
-            //console.log('CANTIDAD Y PRECIO ENVIO: ', CantidadProductoPedido, ' y ', PrecioEnvioPedido)
+            //console.log('CANTIDAD Y PRECIO ENVIO: ', CantidadProductoPedido)
             if (stateApprovalField === 'approved') {
                 const { TotalEnvioPedido } = originalDoc.DetallesPagoPedido
                 //console.log('PRECIO TOTAL ENVIO ORIGEN: ', TotalEnvioPedido)
@@ -19,25 +19,24 @@ const getTotalShippingOrder: FieldHook = async ({ data, originalDoc }) => {
             } else {
 
                 const clientLocactionFieldId = data.ClienteIdPedido
-                const clientLocationResponse = await payload.find({
+                const clientLocationResponse = await payload.findByID({
                     collection: 'clientes',
-                    where: {
                         id: clientLocactionFieldId
-                    }
                 })
-                if (clientLocationResponse.docs && clientLocationResponse.docs.length > 0) {
-                    const clientLocationData = clientLocationResponse.docs[0].UbicacionCliente
+                let clientLocationData: unknown
+                if (clientLocationResponse) {
+                    clientLocationData = clientLocationResponse.UbicacionCliente
 
                     const formatLocationPriceData = (ubicacionCliente: UbicacionInterface): number => {
                         const { value } = ubicacionCliente
                         return value.PrecioEnvioUbicacion
                     }
                     
-                    let locationDataNumber: LocationPriceType = 0
+                    let locationPrice: LocationPriceType = 0
                     if (clientLocationData) {
                         const getLocationPrice = formatLocationPriceData(clientLocationData as UbicacionInterface)
-                        locationDataNumber = getLocationPrice
-                        const totalShippingPrice = Number(CantidadProductoPedido * locationDataNumber)
+                        locationPrice = getLocationPrice
+                        const totalShippingPrice = Number(CantidadProductoPedido * locationPrice)
                         return totalShippingPrice
                     }
                 }
